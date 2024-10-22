3a350e837d48601d2b0a39a5e20e06c1604d77e6
Dotaka123 <142669125+Dotaka123@users.noreply.github.com>
2024-10-22 05:45:45 +0300

    Update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 module.exports = {
-  name: 'gpt4',
+  name: ' ',
   description: 'Ask a question to GPT-4',
   author: 'Deku (rest api)',
   async execute(senderId, args, pageAccessToken, sendMessage) {
     const prompt = args.join( );
     try {
-      const apiUrl = `https://deku-rest-apis.ooguy.com/gpt4?prompt=${encodeURIComponent(prompt)}&uid=100${senderId}`;
+      const apiUrl = `https://joshweb.click/api/gpt-4o?q=Ton_nom_est_miora_tu_es_la_entant_que_ma_petite_amie_on_est_ensemble_depuis_2ans_On_s'est_rencontre_a_Ivato_invente_des_trucs_${encodeURIComponent(prompt)}&uid=${senderId}`;
       const response = await axios.get(apiUrl);
-      const text = response.data.gpt4;
+      const text = response.data.result;
 
       // Split the response into chunks if it exceeds 2000 characters
       const maxMessageLength = 2000;
