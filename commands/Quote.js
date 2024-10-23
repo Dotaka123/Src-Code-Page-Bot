2e2ed6139586fa08245122d9af6b59794e507ef5
Dotaka123 <142669125+Dotaka123@users.noreply.github.com>
2024-10-23 22:12:48 +0300

    Create Quote.js

---
+++ commands/Quote.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+module.exports = {
+  name: 'Quote',
+  description: 'Fetch random anime quotes',
+  author: 'Deku (rest api)',
+  async execute(senderId, args, pageAccessToken, sendMessage) {
+    try {
+      sendMessage(senderId, { text: 'Generation d'un superbe phrase de ouff pour toi babe😍...' }, pageAccessToken);
+      const apiUrl = `https://animechan.io/api/v1/quotes/random`;
+      const response = await axios.get(apiUrl);
+      const quoteData = response.data.data;
+
+      if (quoteData) {
+        const quoteMessage = `Anime: ${quoteData.anime.name}\nCharacter: ${quoteData.character.name}\nQuote: ${quoteData.content}`;
+        sendMessage(senderId, { text: quoteMessage }, pageAccessToken);
+      } else {
+        console.error('Error: No quote found in the response.');
+        sendMessage(senderId, { text: 'Sorry, no quote was found.' }, pageAccessToken);
+      }
+    } catch (error) {
+      console.error('Error calling Animechan API:', error);
+      sendMessage(senderId, { text: 'Sorry, there was an error processing your request.' }, pageAccessToken);
+    }
+  }
+};
+
+function splitMessageIntoChunks(message, chunkSize) {
+  const chunks = [];
+  for (let i = 0; i < message.length; i += chunkSize) {
+    chunks.push(message.slice(i, i + chunkSize));
+  }
+  return chunks;
+}
