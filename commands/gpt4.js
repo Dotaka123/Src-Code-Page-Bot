1a9bdee220c1a08f0fc50f736c119948f32d6dc5
Dotaka123 <142669125+Dotaka123@users.noreply.github.com>
2024-10-24 07:27:18 +0300

    Update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 module.exports = {
+  name: ' ',
   description: 'Ask a question to GPT-4',
   author: 'Deku (rest api)',
   async execute(senderId, args, pageAccessToken, sendMessage) {
