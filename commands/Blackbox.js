270b7ff1a7f91089bcf0d7d406dc5bababd95e37
Dotaka123 <142669125+Dotaka123@users.noreply.github.com>
2024-10-31 22:11:48 +0300

    Update and rename Llama.js to Blackbox.js

---
+++ commands/Blackbox.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+const fs = require('fs');
+
+const token = fs.readFileSync('token.txt', 'utf8');
+
+module.exports = {
+  name: 'blackbox',
+  description: 'Discuter avec blackbox',
+  author: 'Tata',
+  usage:'blackbox [ta question]',
+
+  async execute(senderId, args) {
+    const pageAccessToken = token;
+    const input = (args.join(' ') || 'hi').trim();
+    const modifiedPrompt = `${input}, direct answer.`;
+
+    try {
+      sendMessage(senderId, { text: '...✍🏻' }, pageAccessToken);
+      const response = await axios.get(`https://www.geo-sevent-tooldph.site/api/blackbox?prompt=${encodeURIComponent(modifiedPrompt)}`);
+      const data = response.data;
+      const formattedMessage = `・──🤖blackbox🤖──・\n${data.message.content}\n・──── >ᴗ< ────・`;
+
+      await sendMessage(senderId, { text: formattedMessage }, pageAccessToken);
+    } catch (error) {
+      console.error('Error:', error);
+      await sendMessage(senderId, { text: 'Error: Unexpected error.' }, pageAccessToken);
+    }
+  }
+};
