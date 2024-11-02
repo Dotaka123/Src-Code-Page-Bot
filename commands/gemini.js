728678c776035b402a55f1cd35043f3b2ac2d2e6
Dotaka123 <142669125+Dotaka123@users.noreply.github.com>
2024-11-02 11:52:21 +0300

    Update gemini.js

--- commands/gemini.js
+++ commands/gemini.js
@@ -5,13 +5,13 @@ module.exports = {
   name: 'gemini',
   description: 'Interact with Google Gemini',
   usage: 'gemini [your message]',
-  author: 'coffee',
+  author: 'tata',
   async execute(senderId, args, pageAccessToken) {
     const prompt = args.join(' ');
     if (!prompt) return sendMessage(senderId, { text: "Usage: gemini <your message>" }, pageAccessToken);
 
     try {
-      const { data } = await axios.get(`https://joshweb.click/gemini?prompt=${encodeURIComponent(prompt)}`);
+      const { data } = await axios.get(`https://rest-api-production-5054.up.railway.app/google?prompt=${encodeURIComponent(prompt)}&model=gemini-1.5-flash&uid=${senderId}`);
       sendMessage(senderId, { text: data.gemini }, pageAccessToken);
     } catch {
       sendMessage(senderId, { text: 'Error generating response. Try again later.' }, pageAccessToken);
