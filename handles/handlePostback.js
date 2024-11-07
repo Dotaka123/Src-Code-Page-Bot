7d6a545826177c10c689a95fa39d2f20fe94d16b
Dotaka123 <142669125+Dotaka123@users.noreply.github.com>
2024-11-07 22:07:15 +0300

    Update handlePostback.js

--- handles/handlePostback.js
+++ handles/handlePostback.js
@@ -9,9 +9,26 @@ const handlePostback = async (event, pageAccessToken) => {
   }
 
   try {
-    // Check if the payload is 'GET_STARTED' to send a welcome message
+    // Check if the payload is 'GET_STARTED' to send the custom welcome message
     if (payload === 'WELCOME_MESSAGE') {
-      await sendMessage(senderId, { text: 'Bienvenue sur notre bot ! /n Envoyez [help] pour voir les commandes du bot /n Pour soutenir le bot,contactez l admin www.facebook.com/lahatra.gameur /n Ou Envoyer par Mvola (0344322638)' }, pageAccessToken);
+      const welcomeMessage = `
+      🇫🇷: Bienvenue dans l'univers de Girlfriend AI, votre petite amie virtuelle 🌟! 
+      Préparez-vous à vivre une expérience unique et amusante avec moi 🤖💕! 
+      Ensemble, nous allons créer des moments inoubliables et partager des rires, des conseils et des discussions passionnantes. 
+      Je suis là pour vous, 24/7, pour rendre votre vie plus agréable et colorée 🌈! 
+      Alors, prêt à démarrer cette aventure palpitante avec moi, votre petite amie virtuelle préférée? 💌💬🌠
+      Envoyez "help" pour voir toutes mes fonctionnalités.
+
+      🇳🇿: Welcome to the world of Girlfriend AI, your virtual girlfriend 🌟! 
+      Get ready to experience something unique and fun with me 🤖💕! 
+      Together, we will create unforgettable moments and share laughter, advice, and exciting conversations. 
+      I am here for you, 24/7, to make your life more enjoyable and colorful 🌈! 
+      So, are you ready to start this thrilling adventure with me, your favorite virtual girlfriend? 💌💬
+      Type "help" to see all my features.
+      `;
+
+      // Send the welcome message
+      await sendMessage(senderId, { text: welcomeMessage.trim() }, pageAccessToken);
     } else {
       // For other postback payloads, send a default response
       await sendMessage(senderId, { text: `Vous avez envoyé un postback avec le payload : ${payload}` }, pageAccessToken);
