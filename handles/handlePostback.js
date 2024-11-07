2800704fd662d6599cdda4180da811dbb6fb021d
Dotaka123 <142669125+Dotaka123@users.noreply.github.com>
2024-11-07 21:47:17 +0300

    Update handlePostback.js

--- handles/handlePostback.js
+++ handles/handlePostback.js
@@ -4,10 +4,18 @@ const handlePostback = async (event, pageAccessToken) => {
   const { id: senderId } = event.sender || {};
   const { payload } = event.postback || {};
 
-  if (!senderId || !payload) return console.error('Invalid postback event object');
+  if (!senderId || !payload) {
+    return console.error('Invalid postback event object');
+  }
 
   try {
-    await sendMessage(senderId, { text: `You sent a postback with payload: ${payload}` }, pageAccessToken);
+    // Check if the payload is 'GET_STARTED' to send a welcome message
+    if (payload === 'GET_STARTED') {
+      await sendMessage(senderId, { text: 'Bienvenue sur notre bot ! /n Envoyez [help] pour voir les commandes du bot /n Pour soutenir le bot,contactez l admin www.facebook.com/lahatra.gameur /n Ou Envoyer par Mvola (0344322638)' }, pageAccessToken);
+    } else {
+      // For other postback payloads, send a default response
+      await sendMessage(senderId, { text: `Vous avez envoyé un postback avec le payload : ${payload}` }, pageAccessToken);
+    }
   } catch (err) {
     console.error('Error sending postback response:', err.message || err);
   }
