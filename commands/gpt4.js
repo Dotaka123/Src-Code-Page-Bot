c4570e7b0932da34a5fdb668d19564282b38084c
Dotaka123 <142669125+Dotaka123@users.noreply.github.com>
2024-11-21 20:34:54 +0300

    Update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -37,7 +37,7 @@ module.exports = {
     const mode = userPreferences[senderId] || 'fille';
     const characterPrompt = prompts[mode];
     const modifiedPrompt = `${input}, direct answer.`;
-    const senderId = id;
+    
 
     try {
       // Message d'attente
@@ -45,7 +45,7 @@ module.exports = {
 
       // Requête API avec le prompt personnalisé pour GPT-4
       const response = await axios.get(
-        `https://ccprojectapis.ddns.net/api/gpt4o?ask=${encodeURIComponent(characterPrompt)}_${encodeURIComponent(modifiedPrompt)}&id=${id}`
+        `https://ccprojectapis.ddns.net/api/gpt4o?ask=${encodeURIComponent(characterPrompt)}_${encodeURIComponent(modifiedPrompt)}&id=${encodeURIComponent(senderId)}`
       );
       const data= response.data
       const messageText = data.response;
